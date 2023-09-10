94a77fc1d565d24968da5a1d583796d319dc3cef
Justin <138933736+JustinDerengo@users.noreply.github.com>
2023-09-10 16:29:07 -0600

    Driver ShapeArray Class

--- src/shapes/ShapeArray.java
+++ src/shapes/ShapeArray.java
@@ -3,7 +3,19 @@ package shapes;
 public class ShapeArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 Shape[] shapeArray = new Shape[3];
+	        
+	        Sphere sphere = new Sphere(5.0);
+	        Cylinder cylinder = new Cylinder(3.0, 7.0);
+	        Cone cone = new Cone(4.0, 6.0);
+
+	        shapeArray[0] = sphere;
+	        shapeArray[1] = cylinder;
+	        shapeArray[2] = cone;
+
+	        for (Shape shape : shapeArray) {
+	            System.out.println(shape.toString());
+	        }
 
 	}
 
